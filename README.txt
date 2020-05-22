9514d6e9fa03a528778d00b6b64ae3d074feb5af
Ne-el <neel.mittal101@gmail.com>
2020-05-22 10:46:00 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+Please run wget install
+Before you do anything, put the machine's users in the "list1.txt" file and the users in the readme in "list2.txt"
---
+++ WindowsHardening.bat
@@ -0,0 +1,342 @@
+::Check for admin rights.
+@echo off
+echo Checking if script contains Administrative rights...
+net sessions
+if %errorlevel%==0 (
+echo Success!
+) else (
+echo No admin, please run with Administrative rights...
+pause
+exit
+)
+::Install software
+cd resources
+tcpview.exe
+wget-1.11.4-1-setup.exe
+mbar-1.10.3.1001.exe
+cd ..
+::Compare the users in the README to the users on the machine (You will need to input the list in the list1 and list2 files)
+echo Before this, make sure you have input the machine's users in list1, and the README users in list2.
+pause
+for /f "delims=" %%A in (list1.txt) do @find "%%A" "list2.txt" >nul2>nul || echo.%%A >>notallowed.txt
+::Edit the Audit Policy
+echo Setting auditing success and failure for all categories
+pause
+auditpol /set /category:* /success:enable
+auditpol /set /category:* /failure:enable
+echo Set auditing success and failure
+::Output all current processes to files in root
+cd C:\
+echo "STARTING TO OUTPUT PROCESS FILES DIRECTLY TO THE C:\ DRIVE!"
+pause
+wmic process list brief > BriefProcesses.txt
+if %errorlevel%==1 echo Brief Processes failed to write
+pause
+wmic process list full >FullProcesses.txt
+if %errorlevel%==1 echo Full Processes failed to write
+pause
+wmic startup list full > StartupLists.txt
+if %errorlevel%==1 echo Startup Processes failed to write
+pause
+net start > StartedProcesses.txt
+if %errorlevel%==1 echo Started processes failed to write
+pause
+reg export HKLM\Software\Microsoft\Windows\CurrentVersion\Run  Run.reg
+pause
+if %errorlevel%==1 echo Run processes failed to write
+pause
+::Password Policies
+echo "OUTPUT DONE, CHANGING PASSWORD POLICIES!"
+::Edit set password policies (Can be changed by editing the secconfig.cfg file in resources)
+secedit.exe /configure /db %windir%\securitynew.sdb /cfg resources/secconfig.cfg /areas SECURITYPOLICY
+::Set Lockout Duration
+net accounts /lockoutduration:30
+::Set Lockout Threshold
+net accounts /lockoutthreshold:5
+::Set Lockout Window
+net accounts /lockoutwindow:30
+REM Display current password policy
+echo "CURRENT POLICY"
+PAUSE
+net accounts
+PAUSE
+::Find files
+@echo off
+color 0f
+cls
+echo Flashing Disk to .flashed Files to reference....
+dir /b /s "C:\Program Files\" >> programfiles.flashed
+dir /b /s "C:\Program Files (x86)\" >> programfiles.flashed
+echo Program Files flashed
+dir /b /s "C:\Users\" > users.flashed
+dir /b /s "C:\Documents and Settings" >> users.flashed
+echo User profiles flashed
+dir /b /s "C:\" > c.flashed
+echo C:\ Flashed
+pause
+echo Finding media files in C:\Users and/or C:\Documents and Settings...
+findstr .mp3 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp3 > media_audio
+findstr .ac3 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.ac3 >> media_audio
+findstr .aac users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.aac >> media_audio
+findstr .aiff users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.aiff >> media_audio
+findstr .flac users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.flac >> media_audio
+findstr .m4a users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m4a >> media_audio
+findstr .m4p users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m4p >> media_audio
+findstr .midi users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.midi >> media_audio
+findstr .mp2 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp2 >> media_audio
+findstr .m3u users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.m3u >> media_audio
+findstr .ogg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.ogg >> media_audio
+findstr .vqf users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.vqf >> media_audio
+findstr .wav users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.wav >> media_audio
+findstr .wma users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.wma >> media_video
+findstr .mp4 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.mp4 >> media_video
+findstr .avi users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.avi >> media_video
+findstr .mpeg4 users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .mpeg4 >> media_video
+::BREAKLINE
+findstr .gif users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.gif >> media_pics
+findstr .png users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.png >> media_pics
+findstr .bmp users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ *.bmp >> media_pics
+findstr .jpg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .jpg >> media_pics
+findstr .jpeg users.flashed >NUL
+if %errorlevel%==0 where /r c:\Users\ .jpeg >> media_pics
+C:\WINDOWS\system32\notepad.exe media_video
+C:\WINDOWS\system32\notepad.exe media_audio
+C:\WINDOWS\system32\notepad.exe media_pics
+echo Finding Hacktools now...
+findstr "Cain" programfiles.flashed
+if %errorlevel%==0 (
+echo Cain detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "nmap" programfiles.flashed
+if %errorlevel%==0 (
+echo Nmap detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "keylogger" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential keylogger detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "Armitage" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Armitage detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "Metasploit" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Metasploit framework detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+findstr "Shellter" programfiles.flashed
+if %errorlevel%==0 (
+echo Potential Shellter detected. Please take note, then press any key.
+pause >NUL
+)
+cls
+pause
+::No Remote Desktop
+echo "DISABLING REMOTE DESKTOP"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
+::Windows AUTO-UPDATES
+
+::Remove insecure services
+echo "DISABLING WEAK SERVICES"
+dism /online /disable-feature /featurename:IIS-WebServerRole
+dism /online /disable-feature /featurename:IIS-WebServer
+dism /online /disable-feature /featurename:IIS-CommonHttpFeatures
+dism /online /disable-feature /featurename:IIS-HttpErrors
+dism /online /disable-feature /featurename:IIS-HttpRedirect
+dism /online /disable-feature /featurename:IIS-ApplicationDevelopment
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility45
+dism /online /disable-feature /featurename:IIS-HealthAndDiagnostics
+dism /online /disable-feature /featurename:IIS-HttpLogging
+dism /online /disable-feature /featurename:IIS-LoggingLibraries
+dism /online /disable-feature /featurename:IIS-RequestMonitor
+dism /online /disable-feature /featurename:IIS-HttpTracing
+dism /online /disable-feature /featurename:IIS-Security
+dism /online /disable-feature /featurename:IIS-URLAuthorization
+dism /online /disable-feature /featurename:IIS-RequestFiltering
+dism /online /disable-feature /featurename:IIS-IPSecurity
+dism /online /disable-feature /featurename:IIS-Performance
+dism /online /disable-feature /featurename:IIS-HttpCompressionDynamic
+dism /online /disable-feature /featurename:IIS-WebServerManagementTools
+dism /online /disable-feature /featurename:IIS-ManagementScriptingTools
+dism /online /disable-feature /featurename:IIS-IIS6ManagementCompatibility
+dism /online /disable-feature /featurename:IIS-Metabase
+dism /online /disable-feature /featurename:IIS-HostableWebCore
+dism /online /disable-feature /featurename:IIS-StaticContent
+dism /online /disable-feature /featurename:IIS-DefaultDocument
+dism /online /disable-feature /featurename:IIS-DirectoryBrowsing
+dism /online /disable-feature /featurename:IIS-WebDAV
+dism /online /disable-feature /featurename:IIS-WebSockets
+dism /online /disable-feature /featurename:IIS-ApplicationInit
+dism /online /disable-feature /featurename:IIS-ASPNET
+dism /online /disable-feature /featurename:IIS-ASPNET45
+dism /online /disable-feature /featurename:IIS-ASP
+dism /online /disable-feature /featurename:IIS-CGI
+dism /online /disable-feature /featurename:IIS-ISAPIExtensions
+dism /online /disable-feature /featurename:IIS-ISAPIFilter
+dism /online /disable-feature /featurename:IIS-ServerSideIncludes
+dism /online /disable-feature /featurename:IIS-CustomLogging
+dism /online /disable-feature /featurename:IIS-BasicAuthentication
+dism /online /disable-feature /featurename:IIS-HttpCompressionStatic
+dism /online /disable-feature /featurename:IIS-ManagementConsole
+dism /online /disable-feature /featurename:IIS-ManagementService
+dism /online /disable-feature /featurename:IIS-WMICompatibility
+dism /online /disable-feature /featurename:IIS-LegacyScripts
+dism /online /disable-feature /featurename:IIS-LegacySnapIn
+dism /online /disable-feature /featurename:IIS-FTPServer
+dism /online /disable-feature /featurename:IIS-FTPSvc
+dism /online /disable-feature /featurename:IIS-FTPExtensibility
+dism /online /disable-feature /featurename:TFTP
+dism /online /disable-feature /featurename:TelnetClient
+dism /online /disable-feature /featurename:TelnetServer
+::System INTEG scan
+echo "STARTING SYSTEM INTERGRITY SCAN"
+echo "If it fails make sure you can access Sfc.exe"
+Sfc.exe /scannow
+::Firewall
+echo Enabling firewall (make sure group policy is allowing modifications to the firewall)
+netsh advfirewall set allprofiles state on
+echo Firewall enabled
+echo Setting basic firewall rules..
+netsh advfirewall firewall set rule name="Remote Assistance (DCOM-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (PNRP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (RA Server TCP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP TCP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP UDP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (TCP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Telnet Server" new enable=no 
+netsh advfirewall firewall set rule name="netcat" new enable=no
+echo Set basic firewall rules
+PAUSE
+::REGEDIT
+echo Managing registry keys...
+::Restrict CD ROM drive
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateCDRoms /t REG_DWORD /d 1 /f
+::Disallow remote access to floppy disks
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
+::Disable auto Admin logon
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+::Clear page file (Will take longer to shutdown)
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+::Prevent users from installing printer drivers 
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
+::Add auditing to Lsass.exe
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\LSASS.exe" /v AuditLevel /t REG_DWORD /d 00000008 /f
+::Enable LSA protection
+reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v RunAsPPL /t REG_DWORD /d 00000001 /f
+::Limit use of blank passwords
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+::Auditing access of Global System Objects
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v auditbaseobjects /t REG_DWORD /d 1 /f
+::Auditing Backup and Restore
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v fullprivilegeauditing /t REG_DWORD /d 1 /f
+::Restrict Anonymous Enumeration #1
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f
+::Restrict Anonymous Enumeration #2
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f
+::Disable storage of domain passwords
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f
+::Take away Anonymous user Everyone permissions
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f
+::Allow Machine ID for NTLM
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v UseMachineId /t REG_DWORD /d 0 /f
+::Do not display last user on logon
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+::Enable UAC
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
+::UAC setting (Prompt on Secure Desktop)
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
+::Enable Installer Detection
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
+::Disable undocking without logon
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v undockwithoutlogon /t REG_DWORD /d 0 /f
+::Enable CTRL+ALT+DEL
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 0 /f
+::Max password age
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v MaximumPasswordAge /t REG_DWORD /d 15 /f
+::Disable machine account password changes
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v DisablePasswordChange /t REG_DWORD /d 1 /f
+::Require strong session key
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireStrongKey /t REG_DWORD /d 1 /f
+::Require Sign/Seal
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireSignOrSeal /t REG_DWORD /d 1 /f
+::Sign Channel
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SignSecureChannel /t REG_DWORD /d 1 /f
+::Seal Channel
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SealSecureChannel /t REG_DWORD /d 1 /f
+::Set idle time to 45 minutes
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v autodisconnect /t REG_DWORD /d 45 /f
+::Require Security Signature - Disabled pursuant to checklist:::
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v enablesecuritysignature /t REG_DWORD /d 0 /f
+::Enable Security Signature - Disabled pursuant to checklist:::
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v requiresecuritysignature /t REG_DWORD /d 0 /f
+::Clear null session pipes
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
+::Restict Anonymous user access to named pipes and shares
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
+::Encrypt SMB Passwords
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
+::Clear remote registry paths
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
+::Clear remote registry paths and sub-paths
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
+::Enable smart screen for IE8
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\PhishingFilter" /v EnabledV8 /t REG_DWORD /d 1 /f
+::Enable smart screen for IE9 and up
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\PhishingFilter" /v EnabledV9 /t REG_DWORD /d 1 /f
+::Disable IE password caching
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v DisablePasswordCaching /t REG_DWORD /d 1 /f
+::Warn users if website has a bad certificate
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonBadCertRecving /t REG_DWORD /d 1 /f
+::Warn users if website redirects
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnOnPostRedirect /t REG_DWORD /d 1 /f
+::Enable Do Not Track
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v DoNotTrack /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Download" /v RunInvalidSignatures /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_LOCALMACHINE_LOCKDOWN\Settings" /v LOCALMACHINE_CD_UNLOCK /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonZoneCrossing /t REG_DWORD /d 1 /f
+::Show hidden files
+reg ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f
+::Disable sticky keys
+reg ADD "HKU\.DEFAULT\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f
+::Show super hidden files
+reg ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSuperHidden /t REG_DWORD /d 1 /f
+::Disable dump file creation
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 0 /f
+::Disable autoruns
+reg ADD HKCU\SYSTEM\CurrentControlSet\Services\CDROM /v AutoRun /t REG_DWORD /d 1 /f
+echo Managed registry keys
+pause
+::Windows UPDATES
+UsoClient StartInstallWait
